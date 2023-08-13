8c468f4c2e545b35ed307b020af862e00342068b
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-13 20:11:46 +0300

    dist

---
+++ dist/server/_empty-middleware.mjs
@@ -0,0 +1,12 @@
+export { o as onRequest } from './renderers.mjs';
+import 'html-escaper';
+import 'path-to-regexp';
+import 'cookie';
+import '@astrojs/internal-helpers/path';
+import 'kleur/colors';
+import 'node:fs';
+import 'node:http';
+import 'node:tls';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
---
+++ dist/server/chunks/resume@_@astro.d693418a.mjs
@@ -0,0 +1,16 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'path-to-regexp';
+import 'cookie';
+import '@astrojs/internal-helpers/path';
+import 'kleur/colors';
+import 'node:fs';
+import 'node:http';
+import 'node:tls';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./prerender.55e9013a.mjs').then(n => n.r);
+
+export { page };
