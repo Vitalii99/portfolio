87e96b0058499fd6e6c14918827c34d0afc0dee2
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-13 19:04:28 +0300

    astroconfigmjs build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig, sharpImageService } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [tailwind()],
   experimental: {
     assets: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
