cf26c56078567ff3d8e64004eb358e2c3faf2741
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-14 00:10:33 +0300

    netlify start 13082023

---
+++ .netlify/functions-internal/chunks/resume@_@astro.d0be4fcc.mjs
@@ -0,0 +1,13 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./pages/resume.astro.25a1ec56.mjs');
+
+export { page };
