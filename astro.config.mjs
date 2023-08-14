54c698858444c318e3287c905e096b5940b09d1e
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-14 22:07:09 +0300

    experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
