65e56de19f76d69e0ae6070bacdec72d44ab4902
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-15 00:34:54 +0300

    projects

---
+++ .netlify/functions-internal/chunks/pages/resume.astro.c03cf54e.mjs
@@ -0,0 +1,33 @@
+/* empty css                         */import { c as createAstro, a as createComponent, r as renderTemplate, f as renderComponent, m as maybeRenderHead } from '../../renderers.mjs';
+import 'html-escaper';
+import { a as $$Layout } from './404.astro.18f7f3b8.mjs';
+import { $ as $$Header, a as $$Footer } from './index.astro.cdc7e995.mjs';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+/* empty css                         *//* empty css                           *//* empty css                           */
+const $$Astro = createAstro();
+const $$Resume = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Resume;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Vitalii Pavlivskyi Resume" }, { "default": ($$result2) => renderTemplate`
+  ${renderComponent($$result2, "Header", $$Header, {})}
+  ${maybeRenderHead()}<main class="w-full xl:w-container mx-auto mt-24 px-4 xl:px-0">
+    <h1 class="text-3xl font-medium">Vitalii Pavlivskyi</h1>
+    <p class="text-pink-500">
+      To get started, open the directory <code>src/pages</code> in your project.<br>
+      <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.
+    </p>
+    ${renderComponent($$result2, "Footer", $$Footer, {})}
+  </main>
+` })}`;
+}, "D:/coder/ASTRO_PORTFOLIO/my-portfolio/src/pages/resume.astro", void 0);
+
+const $$file = "D:/coder/ASTRO_PORTFOLIO/my-portfolio/src/pages/resume.astro";
+const $$url = "/resume";
+
+export { $$Resume as default, $$file as file, $$url as url };
