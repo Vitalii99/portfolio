3dde35d38e3de7a4a066e0d121ceb6b8a08da1ea
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-17 19:53:28 +0300

    auto photo last

---
+++ .netlify/functions-internal/chunks/index@_@astro.a8479189.mjs
@@ -0,0 +1,13 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./pages/index.astro.9a6bb5f9.mjs').then(n => n.i);
+
+export { page };
