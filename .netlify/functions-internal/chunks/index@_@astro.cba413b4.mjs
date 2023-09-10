d2fae70438f60fcab5096725bd3f9fbd80227e93
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-09-11 00:57:56 +0300

    done freelancer

---
+++ .netlify/functions-internal/chunks/index@_@astro.cba413b4.mjs
@@ -0,0 +1,13 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./prerender.a9b71842.mjs').then(n => n.i);
+
+export { page };
