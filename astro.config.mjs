b31a81939460b626b1ac635ed9fe500772d395c3
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-09-13 05:19:40 +0300

    port 3099

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 4321
+    port: 3099
   },
   output: 'server',
   adapter: netlify(),
