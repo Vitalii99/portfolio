cbfa44f1c5fd3ffe231962159a2cdd517b4873bd
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-09-13 03:39:37 +0300

    added manifest.json file PWA

---
+++ .netlify/functions-internal/chunks/404@_@astro.e7c4ba4f.mjs
@@ -0,0 +1,13 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./prerender.0c811d82.mjs').then(n => n._);
+
+export { page };
