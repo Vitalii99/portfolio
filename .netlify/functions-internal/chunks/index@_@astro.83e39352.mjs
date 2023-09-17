8879f5928c1f63354ce8e2379e6ea1cc5ba748fd
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-09-18 01:14:46 +0300

    new build

---
+++ .netlify/functions-internal/chunks/index@_@astro.83e39352.mjs
@@ -0,0 +1,13 @@
+export { o as onRequest, renderers } from '../renderers.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'mime/lite.js';
+
+const page = () => import('./prerender.06dd9903.mjs').then(n => n.i);
+
+export { page };
